4b94c8ca8e8d8aafe0ccd13fe09581959b122404
gorgick <gorgick2014@yandex.by>
2025-10-23 10:11:23 +0300

    def download_video

--- youtube.py
+++ youtube.py
@@ -1,11 +1,13 @@
 from pytubefix import YouTube
 from pytubefix.cli import on_progress
 
-if __name__ == '__main__':
-    url = "https://www.youtube.com/watch?v=J0Aq44Pze-w"
 
-    yt = YouTube(url, on_progress_callback=on_progress)
-    print(yt.title)
-
-    ys = yt.streams.get_highest_resolution()
-    ys.download()
+def download_video(url, output_path):
+    try:
+        yt = YouTube(url, on_progress_callback=on_progress)
+        print(yt.title)
+        ys = yt.streams.get_highest_resolution()
+        ys.download(output_path=output_path)
+        print('Downloaded')
+    except Exception as e:
+        print(e)
