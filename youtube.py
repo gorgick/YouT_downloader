dc3940bb5475c3c8001eb201930f9aa37fb3d5d4
gorgick <gorgick2014@yandex.by>
2025-10-25 21:00:51 +0300

    folder for save, interface

--- youtube.py
+++ youtube.py
@@ -1,6 +1,9 @@
 from pytubefix import YouTube
 from pytubefix.cli import on_progress
 
+import tkinter as tk
+from tkinter import filedialog
+
 
 def download_video(url, output_path):
     try:
@@ -11,3 +14,22 @@ def download_video(url, output_path):
         print('Downloaded')
     except Exception as e:
         print(e)
+
+
+def open_file_dialog():
+    folder = filedialog.askdirectory()
+    if folder:
+        print(f"Selected folder {folder}")
+    return folder
+
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.withdraw()
+    video_url = input("Enter a youtube-video url: ")
+    save_dir = open_file_dialog()
+    if save_dir:
+        print("Started download")
+        download_video(video_url, save_dir)
+    else:
+        print("Select a directory for folder")
